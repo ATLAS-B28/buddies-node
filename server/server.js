1e1aab7d5f9e5ca3383721465212a0054b114656
aditya <adityacbhambere28@gmail.com>
2022-11-21 18:22:48 +0530

    hello

--- server/server.js
+++ server/server.js
@@ -1,11 +1,22 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const socketIO = require('socket.io')
+//create our own server using http
+const http = require('http')
 //simplify using path
 const path = require('path')
 const publicpath = path.join(__dirname,"/../public")
 const port = process.env.PORT || 3000
+let server = http.createServer(app)//create an instance
+let io = socketIO(server)
 app.use(express.static(publicpath))
-app.listen(port,()=>{
+io.on("connection",(socket)=>{
+  console.log("New user connected");
+})
+io.on("disconnect",(socket)=>{
+    console.log("User disconnected");
+  })
+server.listen(port,()=>{
     console.log(`Server is running on port ${port}...`)
 })
